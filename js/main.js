068ae323123644318b5e39fccb3bb006805108f6
Klim <klim_somov@mail.ru>
2022-09-09 08:59:07 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const headerMenu = document.querySelector('.header__menu');
+    iconMenu.addEventListener("click", e => {
+        headerMenu.classList.toggle('_active');
+    });
+}
+
+const iconMenub = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const icon = document.querySelector('.menu-icon');
+    iconMenub.addEventListener("click", e => {
+        icon.classList.toggle('_active-b');
+    });
+};
+
+$(function(){
+
+$('.slider').slick();
+
+});
